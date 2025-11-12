f5c0731a59a0d7b52c26b1322df935befa0dfba4
Kyle <canepak@oregonstate.edu>
2025-11-12 09:10:40 -0800

    finished exercise 6

--- main.rb
+++ main.rb
@@ -4,10 +4,12 @@ require_relative 'bike'
 
 bike = Bike.new(1, :pink, 99.99)
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
+cargo = Cargo.new()
 
-puts "Space for #{bike.pannier_remaining_capacity} items left."
+cargo.add_cargo(:apples)
+cargo.add_cargo(:water)
+cargo.add_cargo(:repair_kit)
+
+puts "Space for #{cargo.pannier_remaining_capacity} items left."
 
 bike.rent!
