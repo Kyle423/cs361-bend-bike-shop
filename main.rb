acca1ea293fa1e2a3e28d38491b392d4902ca7cb
Kyle <canepak@oregonstate.edu>
2025-11-20 10:07:31 -0800

    Finished Exercise 7

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require_relative 'rental'
 
 items = [:apple, :water, :protein_bar]
 
-bike = Bike.new(1, :pink, 99.99, items)
+bike = Bike.new(1, :pink, 99.99, Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, items, bike))
 
 rental = Rental.new(bike)
 
